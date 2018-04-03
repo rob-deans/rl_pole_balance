2fd3d829fa743385ed12552f41584f19d5ee5610
Rob Deans <robertdeans@ntlworld.com>
2018-04-04 00:07:59 +0100

    Added continous control for pendulum

--- brain.py
+++ brain.py
@@ -4,6 +4,7 @@ import tensorflow as tf
 import numpy as np
 import random
 import matplotlib.pyplot as plt
+import math as maths
 import pandas as pd
 from scipy.interpolate import spline
 
@@ -98,16 +99,17 @@ class ActorCriticModel:
     def __init__(self, memory, env):
         # General
         self.memory = memory
-        self.discount_factor = 0.99
+        self.discount_factor = .9
 
         # Environment
         self.state_space = env.observation_space.shape[0]
-        self.action_space = env.action_space.n
+        self.action_space = env.action_space.shape[0]
         self.value_size = 1
 
         self.actor_lr = 1e-3
-        self.critic_lr = 5e-3
+        self.critic_lr = 1e-2
 
+        init = tf.uniform_unit_scaling_initializer
         # ============================ #
         #            Actor             #
         # ============================ #
@@ -115,14 +117,33 @@ class ActorCriticModel:
         self.actor_input_action = tf.placeholder(tf.float32, shape=[None, self.action_space], name='actor_i_act')
         self.actor_td_error = tf.placeholder(tf.float32, shape=[None, 1], name='td_placeholder')
 
-        # init = tf.truncated_normal_initializer(0, 0.01)
-        init = tf.uniform_unit_scaling_initializer
+        hidden1 = tf.contrib.layers.fully_connected(
+            inputs=self.actor_input_state,
+            num_outputs=30,
+            activation_fn=tf.nn.relu,
+            weights_initializer=tf.random_normal_initializer(0.,  .01)
+        )
+        self.sigma = tf.contrib.layers.fully_connected(
+            inputs=hidden1,
+            num_outputs=1,
+            activation_fn=tf.nn.softplus,
+            weights_initializer=tf.random_normal_initializer(0.,  .01)
+        )
 
-        net = tf.layers.dense(inputs=self.actor_input_state, units=36, activation=tf.nn.relu, kernel_initializer=init, name='dense_1')
-        self.actor_output = tf.layers.dense(inputs=net, units=self.action_space, kernel_initializer=init, activation=tf.nn.softmax, name='output')
+        self.mu = tf.contrib.layers.fully_connected(
+            inputs=hidden1,
+            num_outputs=1,
+            activation_fn=tf.nn.tanh,
+            weights_initializer=tf.random_normal_initializer(0.,  .01)
+        )
 
-        # Categorical cross entropy
-        loss = tf.log(tf.reduce_sum(tf.multiply(self.actor_input_action, self.actor_output))) * self.actor_td_error
+        self.mu, self.sigma = self.mu*2, self.sigma+0.1
+        self.normal_dist = tf.distributions.Normal(self.mu, self.sigma)
+        self.actor_output = tf.clip_by_value(self.normal_dist.sample(1), env.action_space.low[0], env.action_space.high[0])
+
+        loss = self.normal_dist.log_prob(self.actor_input_action) * self.actor_td_error
+
+        loss += 0.01*self.normal_dist.entropy()
         self.actor_optimise = tf.train.AdamOptimizer(self.actor_lr).minimize(-loss)
 
         # ============================ #
@@ -134,9 +155,9 @@ class ActorCriticModel:
 
         critic_net = self.critic_input_state
 
-        critic_net = tf.layers.dense(inputs=critic_net, units=100, activation=tf.nn.relu, kernel_initializer=init)
-        critic_net = tf.layers.dense(inputs=critic_net, units=150, activation=tf.nn.relu, kernel_initializer=init)
-        critic_net = tf.layers.dense(inputs=critic_net, units=100, activation=tf.nn.relu, kernel_initializer=init)
+        critic_net = tf.layers.dense(inputs=critic_net, units=32, activation=tf.nn.relu, kernel_initializer=init)
+        # critic_net = tf.layers.dense(inputs=critic_net, units=48, activation=tf.nn.relu, kernel_initializer=init)
+        # critic_net = tf.layers.dense(inputs=critic_net, units=32, activation=tf.nn.relu, kernel_initializer=init)
 
         self.critic_output = tf.layers.dense(inputs=critic_net, units=self.value_size, activation=None, kernel_initializer=init)
 
@@ -148,8 +169,7 @@ class ActorCriticModel:
         self.session.run(tf.global_variables_initializer())
 
     def run(self, state):
-        actions = self.session.run(self.actor_output, feed_dict={self.actor_input_state: [state]})[0]
-        return np.random.choice(self.action_space, 1, p=actions)[0]
+        return self.session.run(self.actor_output, feed_dict={self.actor_input_state: [state]})[0][0]
 
     def predict(self, state):
         return self.session.run(self.critic_output, feed_dict={self.critic_input_state: [state]})[0]
@@ -192,8 +212,8 @@ class ActorCriticModel:
 
 class Memory:
     def __init__(self):
-        self.mem = deque(maxlen=2000)
-        self.batch_size = 32
+        self.mem = deque(maxlen=10000)
+        self.batch_size = 1000
 
     def add(self, state, action, reward, done, next_state):
         self.mem.append([state, action, reward, done, next_state])
@@ -235,28 +255,11 @@ class ReplayMemory:
 class Agent:
 
     def __init__(self):
-        self.max_episodes = 1000
-        self.gamma = 0.99
-        self.env = gym.make('CartPole-v0')
+        self.max_episodes = 500
+        self.env = gym.make('Pendulum-v0')
         self.model = ActorCriticModel(Memory(), env=self.env)
         self.render = False
         self.counts = []
-
-    def discount_rewards(self, r):
-        """ take 1D float array of rewards and compute discounted reward """
-        discounted_r = np.zeros_like(r)
-        running_add = 0
-        for t in reversed(xrange(0, len(r))):
-            running_add = running_add * self.gamma + r[t]
-            discounted_r[t] = running_add
-        return discounted_r
-
-    @staticmethod
-    def process_rewards(rews):
-        """Rewards -> Advantages for one episode. """
-
-        # total reward: length of episode
-        return [len(rews)] * len(rews)
 
     def run(self):
         for episode in range(self.max_episodes):
@@ -271,13 +274,10 @@ class Agent:
                 action = self.model.run(current_state)
 
                 next_state, reward, done, _ = self.env.step(action)  # observe the results from the action
+                # reward /= 10
                 count += reward
 
-                if done and count < 200:
-                    reward = -100
-                actions = np.zeros(self.model.action_space)
-                actions[action] = 1
-                self.model.memory.add(current_state, actions, reward, done, next_state)
+                self.model.memory.add(current_state, action, reward, done, next_state)
 
                 self.model.train()
                 current_state = next_state
@@ -286,6 +286,7 @@ class Agent:
             self.counts.append(count)
 
         plt.plot(self.counts)
+        plt.title('1000 batch/10,000 memory')
         plt.show()
 
 
